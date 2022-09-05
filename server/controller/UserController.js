2553dcd2a96495a8dac43c6d3df468b497d98029
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-09-05 15:47:25 +0200

    user is null case fix

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -26,16 +26,20 @@ exports.auth = (req, res, next) => {
 exports.login = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email });
-    let isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (isMatch) {
-      res.status(200).json({
-        id_utilisateur: user._id,
-        token: jwt.sign({ id_utilisateur: user._id }, "RANDOM_TOKEN_SECRET", {
-          expiresIn: "1h",
-        }),
-      });
+    if (user) {
+      let isMatch = await bcrypt.compare(req.body.password, user.password);
+      if (isMatch) {
+        res.status(200).json({
+          id_utilisateur: user._id,
+          token: jwt.sign({ id_utilisateur: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "1h",
+          }),
+        });
+      } else {
+        res.status(400).json({ message: "Invalid password" });
+      }
     } else {
-      res.status(400).json({ message: "Invalid password" });
+      res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
     console.log(error);
