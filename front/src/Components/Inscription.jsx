0fb0e3929eb4b106e2ee471dcd712674c31b6705
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-09-07 10:07:06 +0200

    add singup

--- front/src/Components/Inscription.jsx
+++ front/src/Components/Inscription.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Inscription() {
-  return <div>Inscription</div>;
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    let user = { nom, prenom, email, password };
+    console.log(user);
+  };
+
+  return (
+    <>
+      <h1>Page Inscription</h1>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          placeholder="Jean"
+          value={nom}
+          onChange={(event) => setNom(event.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Paul"
+          value={prenom}
+          onChange={(event) => setPrenom(event.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button> Submit </button>
+      </form>
+    </>
+  );
 }
