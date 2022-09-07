7c563337cc3b28f4b95c94cc4c2abe31406d1174
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-09-07 10:30:37 +0200

    add login

--- front/src/Components/Connexion.jsx
+++ front/src/Components/Connexion.jsx
@@ -1,5 +1,34 @@
 import React from "react";
-
+import { useState } from "react";
 export default function Connexion() {
-  return <div>Connexion</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    let user = { email, password };
+  };
+  return (
+    <>
+      <h1>Page Connexion</h1>
+      <br />
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="pass"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <br />
+        <button> Submit </button>
+      </form>
+    </>
+  );
 }
