f5c5a6a82e3eb7de8bd87d540b2f03d304fbad42
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-09-08 13:58:03 +0200

    get annonce in acceuil

--- front/src/Components/Acceuil.jsx
+++ front/src/Components/Acceuil.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Acceuil() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/annonce")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return <div>Acceuil</div>;
 }
